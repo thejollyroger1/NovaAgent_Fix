3ac36e73e3eb50c50ca5b1847e2f4214264dcec7
thejollyroger1 <thejollyrogerx@me.com>
2015-12-21 16:49:53 -0600

    Update novafix1391.sh
    spacing

--- novafix1391.sh
+++ novafix1391.sh
@@ -5,13 +5,15 @@ if [ -d /usr/share/nova-agent/1.39.1/ ] ; then
     echo "Nova-agent version 1.39.1 appears to be installed, making changes to fix Nova-agent startup"
     touch /usr/lib/systemd/system/nova-agent.service
     echo '[Unit]
-    Description=nova-agent service
-    After=xe-linux-distribution.service
-    [Service]
-    EnvironmentFile=/etc/nova-agent.env
-    ExecStart=/usr/sbin/nova-agent -n -l info /usr/share/nova-agent/nova-agent.py
-    [Install]
-    WantedBy=multi-user.target' > /usr/lib/systemd/system/nova-agent.service
+Description=nova-agent service
+After=xe-linux-distribution.service
+
+[Service]
+EnvironmentFile=/etc/nova-agent.env
+ExecStart=/usr/sbin/nova-agent -n -l info /usr/share/nova-agent/nova-agent.py
+
+[Install]
+WantedBy=multi-user.target' > /usr/lib/systemd/system/nova-agent.service
 
     echo 'LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/share/nova-agent/1.39.1/lib"
 PYTHONPATH="${PYTHONPATH}:/usr/share/nova-agent/1.39.1/lib/python2.6/site-packages:/usr/share/nova-agent/1.39.1/lib/python2.6/"' > /etc/nova-agent.env
